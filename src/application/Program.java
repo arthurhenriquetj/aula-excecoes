cc43f586ca1395e06c86c11a01514a0bed1c95a3
arthurhenriquetj <arthurhenriquetj@gmail.com>
2018-11-23 12:04:18 -0200

    RUIM metodo retornando string

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,15 +36,13 @@ public class Program {
 			System.out.print("Check-Out Date:");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservations dates for update must be future!");
-			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reervation: Check-out date must be after check-in date!");
+			
+			String error = reservation.updateDates(checkIn, checkOut);
+			
+			if(error != null) {
+				System.out.println("Error in reservation: " + error);
 			}
 			else {
-			reservation.updateDates(checkIn, checkOut);			
 			System.out.println("Updated Reservation: " + reservation);
 			}
 		}
